c83db5e2be14b09027c76ce0abe9fc6a64de1d6b
Irineu Junior <irineu_junior@me.com>
2020-07-15 23:18:44 -0300

    removidos using desnecessarios

--- Controllers/CategoriasController.cs
+++ Controllers/CategoriasController.cs
@@ -2,10 +2,8 @@
 using ApiCatalogo.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace ApiCatalogo.Controllers
 {
